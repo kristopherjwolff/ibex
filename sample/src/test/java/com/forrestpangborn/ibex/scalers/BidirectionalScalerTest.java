56c916fc5907c7c88f1eff5545832ccfa845163e
Forrest Pangborn <fpangborn@rhapsody.com>
2013-02-27 10:18:45 -0800

    add some more image scaling tests

---
+++ sample/src/test/java/com/forrestpangborn/ibex/scalers/BidirectionalScalerTest.java
@@ -0,0 +1,107 @@
+package com.forrestpangborn.ibex.scalers;
+
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class BidirectionalScalerTest extends TestCase {
+
+	private BidirectionalScaler scaler;
+	
+	@Override @Before
+	public void setUp() throws Exception {
+		super.setUp();
+		scaler = new BidirectionalScaler();
+	}
+	
+	@Override @After
+	public void tearDown() throws Exception {
+		super.tearDown();
+		scaler = null;
+	}
+	
+	@Test
+	public void testScaleDownSquareToSquare() {
+		Bitmap orig = Bitmap.createBitmap(20, 20, Config.ARGB_8888);
+		Bitmap bmp = scaler.scale(orig, 10, 10);
+		
+		assertEquals(10, bmp.getWidth());
+		assertEquals(10, bmp.getHeight());
+	}
+	
+	@Test
+	public void testEqualDimensions() {
+		Bitmap orig = Bitmap.createBitmap(10, 10, Config.ARGB_8888);
+		Bitmap bmp = scaler.scale(orig, 10, 10);
+		assertEquals(10, bmp.getWidth());
+		assertEquals(10, bmp.getHeight());
+		assertTrue(bmp == orig);
+	}
+	
+	@Test
+	public void testEqualWidth() {
+		Bitmap orig = Bitmap.createBitmap(20, 10, Config.ARGB_8888);
+		Bitmap bmp = scaler.scale(orig, 20, 20);
+		assertEquals(20, bmp.getWidth());
+		assertEquals(10, bmp.getHeight());
+		assertTrue(bmp == orig);
+	}
+	
+	@Test
+	public void testEqualHeight() {
+		Bitmap orig = Bitmap.createBitmap(10, 20, Config.ARGB_8888);
+		Bitmap bmp = scaler.scale(orig, 20, 20);
+		assertEquals(10, bmp.getWidth());
+		assertEquals(20, bmp.getHeight());
+		assertTrue(bmp == orig);
+	}
+	
+	@Test
+	public void testScaleDownWidthBased() {
+		Bitmap orig = Bitmap.createBitmap(40, 10, Config.ARGB_8888);
+		Bitmap bmp = scaler.scale(orig, 20, 20);
+		assertEquals(20, bmp.getWidth());
+		assertEquals(5, bmp.getHeight());
+	}
+	
+	@Test
+	public void testScaleDownHeightBased() {
+		Bitmap orig = Bitmap.createBitmap(10, 40, Config.ARGB_8888);
+		Bitmap bmp = scaler.scale(orig, 20, 20);
+		assertEquals(5, bmp.getWidth());
+		assertEquals(20, bmp.getHeight());
+	}
+	
+	@Test
+	public void testScaleUpSquareToSquare() {
+		Bitmap orig = Bitmap.createBitmap(10, 10, Config.ARGB_8888);
+		Bitmap bmp = scaler.scale(orig, 20, 20);
+		assertEquals(20, bmp.getWidth());
+		assertEquals(20, bmp.getHeight());
+	}
+	
+	@Test
+	public void testScaleUpWidthBased() {
+		Bitmap orig = Bitmap.createBitmap(10, 5, Config.ARGB_8888);
+		Bitmap bmp = scaler.scale(orig, 20, 20);
+		assertEquals(20, bmp.getWidth());
+		assertEquals(10, bmp.getHeight());
+	}
+	
+	@Test
+	public void testScaleUpHeightBased() {
+		Bitmap orig = Bitmap.createBitmap(5, 10, Config.ARGB_8888);
+		Bitmap bmp = scaler.scale(orig, 20, 20);
+		assertEquals(10, bmp.getWidth());
+		assertEquals(20, bmp.getHeight());
+	}
+}

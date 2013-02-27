4b5b2b8047f45b094c8093908eb7a378441335af
Forrest Pangborn <fpangborn@rhapsody.com>
2013-02-27 10:35:44 -0800

    update some tests

--- sample/src/test/java/com/forrestpangborn/ibex/scalers/DownScalerTest.java
+++ sample/src/test/java/com/forrestpangborn/ibex/scalers/DownScalerTest.java
@@ -36,5 +36,60 @@ public class DownScalerTest extends TestCase {
 		
 		assertEquals(10, bmp.getWidth());
 		assertEquals(10, bmp.getHeight());
+		assertTrue(bmp != orig);
+	}
+	
+	@Test
+	public void testNoScaleUp() {
+		Bitmap orig = Bitmap.createBitmap(10, 10, Config.ARGB_8888);
+		Bitmap bmp = scaler.scale(orig, 20, 20);
+		assertEquals(10, bmp.getWidth());
+		assertEquals(10, bmp.getHeight());
+		assertTrue(bmp == orig);
+	}
+	
+	@Test
+	public void testEqualDimensions() {
+		Bitmap orig = Bitmap.createBitmap(10, 10, Config.ARGB_8888);
+		Bitmap bmp = scaler.scale(orig, 10, 10);
+		assertEquals(10, bmp.getWidth());
+		assertEquals(10, bmp.getHeight());
+		assertTrue(bmp == orig);
+	}
+	
+	@Test
+	public void testEqualWidth() {
+		Bitmap orig = Bitmap.createBitmap(20, 10, Config.ARGB_8888);
+		Bitmap bmp = scaler.scale(orig, 20, 20);
+		assertEquals(20, bmp.getWidth());
+		assertEquals(10, bmp.getHeight());
+		assertTrue(bmp == orig);
+	}
+	
+	@Test
+	public void testEqualHeight() {
+		Bitmap orig = Bitmap.createBitmap(10, 20, Config.ARGB_8888);
+		Bitmap bmp = scaler.scale(orig, 20, 20);
+		assertEquals(10, bmp.getWidth());
+		assertEquals(20, bmp.getHeight());
+		assertTrue(bmp == orig);
+	}
+	
+	@Test
+	public void testScaleDownWidthBased() {
+		Bitmap orig = Bitmap.createBitmap(40, 10, Config.ARGB_8888);
+		Bitmap bmp = scaler.scale(orig, 20, 20);
+		assertEquals(20, bmp.getWidth());
+		assertEquals(5, bmp.getHeight());
+		assertTrue(bmp != orig);
+	}
+	
+	@Test
+	public void testScaleDownHeightBased() {
+		Bitmap orig = Bitmap.createBitmap(10, 40, Config.ARGB_8888);
+		Bitmap bmp = scaler.scale(orig, 20, 20);
+		assertEquals(5, bmp.getWidth());
+		assertEquals(20, bmp.getHeight());
+		assertTrue(bmp != orig);
 	}
 }
